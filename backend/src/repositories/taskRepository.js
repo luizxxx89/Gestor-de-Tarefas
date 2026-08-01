c7fb833e2d76f38c223fa35e8756a1d24d699ee9
luizxxx89 <luizlopesxxx@gmail.com>
2026-07-31 23:53:47 -0400

    Update taskRepository.js
    Adiciona estrutura inicial do taskRepository

--- backend/src/repositories/taskRepository.js
+++ backend/src/repositories/taskRepository.js
@@ -1,12 +1,13 @@
 // Importa a conexão com o banco de dados
 const db = require("../config/database");
 
-//Função responsável por inserir uma nova tarefa no banco
+// Função responsável por inserir uma nova tarefa no banco
 const criarTarefa = (tarefa, callback) => {
-   
- const sql = `
-   INSERT INTO tasks (titulo, descricao)
-    VALUES (?, ?)
-`;
+
+};
+
+// Função responsável por buscar todas as tarefas
+const buscarTodasTarefas = (callback) => {
 
 };     
+
